5a4df047baea156f12f5a89782f19be23fde1677
dabaipet <hp7070@sina.com>
2018-12-11 17:50:45 +0800

    后台模板

--- route/api.php
+++ route/api.php
@@ -19,6 +19,6 @@ Route::domain('api', function () {
     Route::rule('user', 'user/index','GET|POST');
     Route::rule('user-info', 'user/info','GET|POST');
     Route::rule('user-choice', 'user/choice','GET|POST');
-    //Route::get(':c/:a', 'api/:c/:a');
+    Route::get(':c/:a', 'api/:c/:a');
     //Route::rule('worker', 'worker/onMessage','GET|POST');
 });
